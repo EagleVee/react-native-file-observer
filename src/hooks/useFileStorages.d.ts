3957e03d322e4d524dd183a8d1f07693163e6ddf
Vu Nguyen <vu.nguyen8@mservice.com.vn>
2022-02-17 14:43:16 +0700

    fix tsconfig.json

---
+++ src/hooks/useFileStorages.d.ts
@@ -0,0 +1,7 @@
+declare const useFileStorages: () => {
+    store: Object;
+    emitStore: (value: Object) => Promise<void>;
+    clearStore: () => Promise<void>;
+};
+export default useFileStorages;
+//# sourceMappingURL=useFileStorages.d.ts.map
---
+++ src/index.d.ts
@@ -0,0 +1,3 @@
+import useFileStorages from './hooks/useFileStorages';
+export { useFileStorages };
+//# sourceMappingURL=index.d.ts.map
