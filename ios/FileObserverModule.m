1ccd0dbc4c005ee9591fc8db512fd52c1d9b0cce
sondhoang <sondhoang@gmail.com>
2022-02-22 18:16:30 +0700

    fix: duplicate event

--- ios/FileObserverModule.m
+++ ios/FileObserverModule.m
@@ -16,47 +16,48 @@
 
 @implementation FileObserverModule
 {
-  BOOL _hasListeners;
+    BOOL _hasListeners;
+    TABFileMonitor *fileMonitor;
 }
 
 - (instancetype)init {
-  if (self = [super init]) {
-    // Initialize self
-    // Create the test file URL
-  }
-  return self;
+    if (self = [super init]) {
+        // Initialize self
+        // Create the test file URL
+    }
+    return self;
 }
 
 - (void)fileMonitor:(TABFileMonitor *)fileMonitor didSeeChange:(TABFileMonitorChangeType)changeType
 {
-  if (changeType == TABFileMonitorChangeTypeMetadata)
-  {
-    [self onChange:@"TABFileMonitorChangeTypeMetadata"];
-  }
-  else if (changeType == TABFileMonitorChangeTypeDeleted)
-  {
-    [self onChange:@"TABFileMonitorChangeTypeDeleted"];
-  }
-  else if (changeType == TABFileMonitorChangeTypeSize)
-  {
-    //[self onChange:@"TABFileMonitorChangeTypeSize"];
-  }
-  else if (changeType == TABFileMonitorChangeTypeObjectLink)
-  {
-    [self onChange:@"TABFileMonitorChangeTypeObjectLink"];
-  }
-  else if (changeType == TABFileMonitorChangeTypeRenamed)
-  {
-    [self onChange:@"TABFileMonitorChangeTypeRenamed"];
-  }
-  else if (changeType == TABFileMonitorChangeTypeRevoked)
-  {
-    [self onChange:@"TABFileMonitorChangeTypeRevoked"];
-  }
-  else if (changeType == TABFileMonitorChangeTypeModified)
-  {
-    [self onChange:@"TABFileMonitorChangeTypeModified"];
-  }
+    if (changeType == TABFileMonitorChangeTypeMetadata)
+    {
+        [self onChange:@"TABFileMonitorChangeTypeMetadata"];
+    }
+    else if (changeType == TABFileMonitorChangeTypeDeleted)
+    {
+        [self onChange:@"TABFileMonitorChangeTypeDeleted"];
+    }
+    else if (changeType == TABFileMonitorChangeTypeSize)
+    {
+        //[self onChange:@"TABFileMonitorChangeTypeSize"];
+    }
+    else if (changeType == TABFileMonitorChangeTypeObjectLink)
+    {
+        [self onChange:@"TABFileMonitorChangeTypeObjectLink"];
+    }
+    else if (changeType == TABFileMonitorChangeTypeRenamed)
+    {
+        [self onChange:@"TABFileMonitorChangeTypeRenamed"];
+    }
+    else if (changeType == TABFileMonitorChangeTypeRevoked)
+    {
+        [self onChange:@"TABFileMonitorChangeTypeRevoked"];
+    }
+    else if (changeType == TABFileMonitorChangeTypeModified)
+    {
+        [self onChange:@"TABFileMonitorChangeTypeModified"];
+    }
 }
 
 RCT_REMAP_METHOD(initWatchFile,
@@ -65,16 +66,19 @@ RCT_REMAP_METHOD(initWatchFile,
                  initWatchFileResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
 {
-  _testFileURL = [[NSURL alloc] initFileURLWithPath:path];
-  
-  NSFileManager *fileManager = [NSFileManager defaultManager];
-  if (![fileManager fileExistsAtPath:path]) {
-    NSData *data = [@"" dataUsingEncoding:NSUTF8StringEncoding];
-    [data writeToFile:path atomically:YES];
-  }
-  eventChange = event;
-  TABFileMonitor *fileMonitor = [[TABFileMonitor alloc] initWithURL:_testFileURL];
-  fileMonitor.delegate = self;
+    _testFileURL = [[NSURL alloc] initFileURLWithPath:path];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if (![fileManager fileExistsAtPath:path]) {
+        NSData *data = [@"" dataUsingEncoding:NSUTF8StringEncoding];
+        [data writeToFile:path atomically:YES];
+    }
+    eventChange = event;
+    if(self->fileMonitor == nil){
+        fileMonitor = [[TABFileMonitor alloc] initWithURL:_testFileURL];
+        
+        fileMonitor.delegate = self;
+    }
 }
 
 RCT_REMAP_METHOD(clearFileToEmptyObject,
@@ -82,55 +86,72 @@ RCT_REMAP_METHOD(clearFileToEmptyObject,
                  clearFileResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
 {
-  NSData *data = [@"" dataUsingEncoding:NSUTF8StringEncoding];
-  [data writeToFile:path atomically:YES];
+    NSData *data = [@"" dataUsingEncoding:NSUTF8StringEncoding];
+    [data writeToFile:path atomically:YES];
 }
 
 RCT_REMAP_METHOD(onStartListenerObserver,
                  startResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
 {
-  
+    
 }
 
 RCT_REMAP_METHOD(onStopListenerObserver,
                  stopResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
 {
-
+    
 }
 
+RCT_REMAP_METHOD(writeText,
+                 filePath:(NSString *)path
+                 fileText:(NSString *)text
+                 stopResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+
+{
+    NSURL* url  = [[NSURL alloc] initFileURLWithPath:path];
+    
+    NSData *dataFromText = [text dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *error;
+    [dataFromText writeToURL:url
+                     options:kNilOptions
+                       error:&error];
+}
+
+
+
 + (BOOL)requiresMainQueueSetup{
-  return NO;
+    return NO;
 }
 
 RCT_EXPORT_MODULE();
 
 - (NSArray<NSString *> *)supportedEvents
 {
-  return @[eventChange];
+    return @[eventChange];
 }
 
 // Will be called when this module's first listener is added.
 - (void)startObserving
 {
-  _hasListeners = YES;
+    _hasListeners = YES;
 }
 
 // Will be called when this module's last listener is removed, or on dealloc.
 - (void)stopObserving
 {
-  _hasListeners = NO;
+    _hasListeners = NO;
 }
 
 - (void)onChange:(NSString *)event
 {
-  NSError *error;
-  NSString *fileContents = [NSString stringWithContentsOfFile:self->_testFileURL encoding:NSUTF8StringEncoding error:&error];
-  
-  if (_hasListeners) {
-    [self sendEventWithName:eventChange body:[NSString stringWithFormat:@"%@",fileContents]];
-  }
+    NSError *error;
+    NSString *fileContents = [NSString stringWithContentsOfFile:self->_testFileURL encoding:NSUTF8StringEncoding error:&error];
+    
+    if (_hasListeners) {
+        [self sendEventWithName:eventChange body:[NSString stringWithFormat:@"%@",fileContents]];
+    }
 }
 
 @end
